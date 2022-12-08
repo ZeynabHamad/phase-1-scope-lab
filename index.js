a8412ab043f2dd2536f0aacbbf4988a9f0cec694
Zeynab Hamad <zaynabhamad950@gmail.com>
2022-12-08 16:43:27 +0300

    compleated

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+
+//1.
+var customerName= "bob";
+
+//2.
+function upperCaseCustomerName(){
+    customerName= customerName.toUpperCase();
+    return customerName;
+}
+
+//3.
+var bestCustomer ;
+function setBestCustomer(){
+    bestCustomer= "not bob";
+    return bestCustomer;
+}
+
+//4.
+function overwriteBestCustomer(){
+    bestCustomer= "maybe bob";
+    return bestCustomer;
+}
+
+//5.
+const leastFavoriteCustomer="";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="Assignment to constant variable.";
+    return leastFavoriteCustomer;
+
+}
